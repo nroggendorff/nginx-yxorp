d686bba76c6e116bdf5619c989e1c95c0203e556
nroggendorff <nroggendorff87@gmail.com>
2024-12-05 18:40:04 -0600

    fix ~/.ssh

--- go-web.sh
+++ go-web.sh
@@ -13,9 +13,9 @@ sudo apt-get update
 
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-git clone https://github.com/nroggendorff/nginx-yxorp.git .
+git clone https://github.com/nroggendorff/nginx-yxorp.git yxorp
 
-docker build . -t nginx-yxorp
+sudo docker build yxorp -f yxorp/Dockerfile -t nginx-yxorp
 
 SESSION_NAME="yxorp"
 
